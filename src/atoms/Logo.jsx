57b20d19b0f4d0d9beb97c1f5a5d1c9e05abc6b6
unknown <abdulmukti40390@gmail.comgit config --global.user.mukti>
2023-12-28 22:30:49 +0700

    fix Logo

--- src/atoms/Logo.jsx
+++ src/atoms/Logo.jsx
@@ -1,22 +1,62 @@
 import React, { useState } from "react";
-import { PiFilmSlateLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
-import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go";
 import AppStore from "../zustand/AppStore";
+import movieDark from "../assets/darkLogo.svg";
+import movieLight from "../assets/lightLogo.svg";
+import sidebarDark from "../assets/sidebarIconDark.svg";
+import sidebarLight from "../assets/sidebarIconLight.svg";
+import sidebarActiveDark from "../assets/sidebarActiveDark.svg";
+import sidebarActiveLight from "../assets/sidebarActiveLight.svg";
 function Logo() {
   const open = AppStore((state) => state.setSidebar);
   const sidebard = AppStore((state) => state.sidebar);
   return (
-    <div className="flex gap-1 items-center dark:text-slate-100">
-      <PiFilmSlateLight className="sm:text-3xl sm:flex hidden text-2xl" />
-      <button className="text-black dark:text-white" onClick={() => open(true)}>
+    <div className="flex gap-1 pr-3 w-24  items-center justify-start dark:text-slate-100 ">
+      <div className="w-7">
+        <img
+          src={movieDark}
+          alt=""
+          className="w-full text-white sm:flex hidden dark:hidden"
+        />
+        <img
+          src={movieLight}
+          alt=""
+          className="w-full  sm:hidden  hidden sm:dark:flex"
+        />
+      </div>
+      <button
+        className="sm:text-black sm:hidden flex sm:dark:text-white text-black dark:text-white"
+        onClick={() => open(true)}
+      >
         {sidebard ? (
-          <GoSidebarCollapse className="sm:hidden text-xl" />
+          <div className="w-6">
+            <img
+              src={sidebarDark}
+              alt=""
+              className=" w-full flex dark:hidden"
+            />
+            <img
+              src={sidebarLight}
+              alt=""
+              className=" w-full  hidden dark:flex"
+            />
+          </div>
         ) : (
-          <GoSidebarExpand className="sm:hidden text-xl" />
+          <div className="w-6">
+            <img
+              src={sidebarActiveDark}
+              alt=""
+              className="w-full flex dark:hidden "
+            />
+            <img
+              src={sidebarLight}
+              alt=""
+              className=" w-full  hidden dark:flex"
+            />
+          </div>
         )}
       </button>
-      <Link to={"/"} className="sm:text-xl text-lg font-semibold">
+      <Link to={"/"} className="outline-none sm:text-xl text-lg font-semibold">
         VieFlix
       </Link>
     </div>
--- src/atoms/Swap.jsx
+++ src/atoms/Swap.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 function Swap() {
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(true);
   const changeMode = () => {
     setDark(!dark);
     if (!dark) {
@@ -12,13 +12,13 @@ function Swap() {
     }
   };
   return (
-    <label className="swap swap-rotate  text-black dark:text-white">
+    <label className="swap swap-rotate">
       {/* this hidden checkbox controls the state */}
       <input type="checkbox" checked={dark} onChange={changeMode} />
 
       {/* sun icon */}
       <svg
-        className="swap-on fill-current sm:w-8 w-7 h-7 sm:h-8"
+        className="swap-on fill-current  sm:w-8 w-7 h-7 sm:h-8"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -31,7 +31,10 @@ function Swap() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
-        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+        <path
+          d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
+          style={{ color: "white" }}
+        />
       </svg>
     </label>
   );
